4f3f530a0173e0f4ed58df7f169f1bfa08e11b90
Pablo Ramirez <104794231+LordPablito@users.noreply.github.com>
2023-09-04 21:38:12 -0600

    Add files via upload

---
+++ Casa2_Project/src/Dispositivos/dispositivo.java
@@ -0,0 +1,69 @@
+package dispositivos;
+
+public class dispositivo {
+	private double altura;
+    private double peso;
+    private String color;
+    private String marca;
+    private boolean encendido;
+
+    public dispositivo (double altura, double peso, String color, String marca) {
+        this.altura = altura;
+        this.peso = peso;
+        this.color = color;
+        this.marca = marca;
+        this.encendido = false; // Por defecto, el electrodoméstico está apagado al crearlo
+    }
+
+    public void encender() {
+        encendido = true;
+        System.out.println("El electrodoméstico ha sido encendido.");
+    }
+
+    public void apagar() {
+        encendido = false;
+        System.out.println("El electrodoméstico ha sido apagado.");
+    }
+
+    public void recibirOrden(String orden) {
+        System.out.println("Se ha recibido la orden: " + orden);
+    }
+
+    // Getters y setters para los atributos
+    public double getAltura() {
+        return altura;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public boolean isEncendido() {
+        return encendido;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+}
---
+++ Casa2_Project/src/Dispositivos/patio.java
@@ -0,0 +1,52 @@
+package dispositivos;
+
+public class patio extends dispositivo{
+	 private boolean podadoraCespedEncendida;
+	    private boolean riegoEncendido;
+	 public patio(double altura, double peso, String color, String marca) {
+		super(altura, peso, color, marca);
+		
+		podadoraCespedEncendida = false;
+        riegoEncendido = false;
+	}
+	public void encenderPodadora() {
+		podadoraCespedEncendida=true;
+		System.out.println("La podadora de cesped esta encendida");
+		
+		
+	}
+	public void apagarPodadora() {
+		podadoraCespedEncendida=false;
+		System.out.println("La podadora de cesped esta apagada");
+		
+		
+	}
+	
+	public void encenderRiego() {
+		  riegoEncendido = true;		
+		  System.out.println("El sistema de riego esta encendido");
+
+		  
+		
+	}
+	public void apagarRiego() {
+		  riegoEncendido = true;		
+		  System.out.println("El sistema de riego esta encendido");
+
+	  
+		
+	}
+	 public void recibirOrdenRiego(String orden) {
+	    	super.recibirOrden(orden);
+
+	        System.out.println("Se ha recibido la orden para el sistema de riego" + orden);
+	    }
+
+	 public void recibirOrdenPodadora(String orden) {
+	    	super.recibirOrden(orden);
+
+	        System.out.println("Se ha recibido la orden para la podadora de cesped " + orden);
+	    }
+	
+
+}
